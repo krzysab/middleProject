f89dad2a75b6a0257f62c99601ca701cd10fab2d
krzysztof sabatowski <krzysztof.sabatowski@gmail.com>
2019-04-07 14:43:29 +0200

    first classes

---
+++ src/main/java/domain/Address.java
@@ -0,0 +1,11 @@
+package domain;
+
+class Address {
+
+    private String street;
+    private int building;
+    private int local;
+    private String city;
+    private String post;
+
+}
---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private int height;
+    private double weight;
+    private Address address;
+
+}
